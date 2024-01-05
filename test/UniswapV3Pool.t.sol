2bfe93cba78f44197b2a8023f4245591274416a6
0xCO2 <cryptohuang88@gmail.com>
2024-01-05 18:00:41 +0800

    commit

--- test/UniswapV3Pool.t.sol
+++ test/UniswapV3Pool.t.sol
@@ -2,11 +2,66 @@
 pragma solidity ^0.8.14;
 
 import "forge-std/Test.sol";
+import "./ERC20Mintable.sol";
+import "../src/UniswapV3Pool.sol";
 
 contract UniswapV3PoolTest is Test {
-    function Setup() public {}
+    ERC20Mintable token0;
+    ERC20Mintable token1;
+    UniswapV3Pool pool;
 
-    function testExample() public {
-        assertTrue(true);
+    bool transferInMintCallback = true;
+    bool transferInSwapCallback = true;
+
+    struct TestCaseParams {
+        uint256 wethBalance;
+        uint256 usdcBalance;
+        int24 currentTick;
+        int24 lowerTick;
+        int24 upperTick;
+        uint128 liquidity;
+        uint160 currentSqrtP;
+        bool shouldTransferInCallback;
+        bool transferInSwapCallback;
+        bool mintLiqudity;
     }
+
+    function setUp() public {
+        token0 = new ERC20Mintable("Ether", "ETH", 18);
+        token1 = new ERC20Mintable("USDC", "USDC", 18);
+    }
+
+    function testMintSuccess() public {
+        TestCaseParams memory params = TestCaseParams({
+            wethBalance: 1 ether,
+            usdcBalance: 5000 ether,
+            currentTick: 85176,
+            lowerTick: 84222,
+            upperTick: 86129,
+            liquidity: 1517882343751509868544,
+            currentSqrtP: 5602277097478614198912276234240,
+            shouldTransferInCallback: true,
+            mintLiqudity: true
+    });
+
+    }
+
+    function setupTestCase(TestCaseParams memory params) internal returns (uint256 poolBalance0, uint256 poolBalance1){
+        // Mint tokens for the test case
+        token0.mint(address(this), params.wethBalance);
+        token1.mint(address(this), params.usdcBalance);
+
+        pool = new UniswapV3Pool(
+            address(token0),
+            address(token1),
+            params.currentSqrtP,
+            params.currentTick
+        );
+
+        if (params.mintLiqudity)
+    }
+
+    // function testExample() public {
+    //     assertTrue(true);
+    // }
 }
